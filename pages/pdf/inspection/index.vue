b0ae55490444adf32f4bcb0bbb37f04962d6296a
eliasroebl <eliasroebl@gmail.com>
2024-05-28 16:57:34 +0200

    Add Document, Header & Footer to index

--- pages/pdf/inspection/index.vue
+++ pages/pdf/inspection/index.vue
@@ -1,19 +1,27 @@
 <template>
-  <pdf-page>
-    <h1 class="text-3xl">Firstname: {{pageData().firstName}}</h1>
-    <h1 class="text-3xl">Lastname: {{pageData().firstName}}</h1>
-    <h1 class="text-3xl">Age: {{pageData().age}}</h1>
-  </pdf-page>
-
-  <pdf-page>
-    <h1 class="text-3xl">Firstname: {{pageData().firstName}}</h1>
-    <h1 class="text-3xl">Lastname: {{pageData().firstName}}</h1>
-    <h1 class="text-3xl">Age: {{pageData().age}}</h1>
-  </pdf-page>
+  <document>
+    <template #header>
+      <pdf-header left="Left" center="Mid" right="Right"/>
+    </template>
+    <pdf-page>
+      <h1 class="text-3xl">Firstname: {{pageData().firstName}}</h1>
+      <h1 class="text-3xl">Lastname: {{pageData().firstName}}</h1>
+      <h1 class="text-3xl">Age: {{pageData().age}}</h1>
+    </pdf-page>
+    <pdf-page>
+      <h1 class="text-3xl">Firstname: {{pageData().firstName}}</h1>
+      <h1 class="text-3xl">Lastname: {{pageData().firstName}}</h1>
+      <h1 class="text-3xl">Age: {{pageData().age}}</h1>
+    </pdf-page>
+    <template #footer>
+      <pdf-footer left="Left" center="Mid" right="Right"/>
+    </template>
+  </document>
 </template>
 
 <script setup lang="ts">
   import { type Schema } from "@/types/pdf/inspection"
   import {usePageData} from "@/composeables/usePageData";
+  import Document from "~/components/pdf/Document.vue";
   let pageData = () => usePageData<Schema>()
 </script>
