6f9b581a5aeb437416918b378bd12255564f5da2
eliasroebl <eliasroebl@gmail.com>
2024-05-28 16:57:14 +0200

    Add Document

---
+++ components/pdf/Document.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="print-content">
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+    <div class="footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DocumentComponent'
+};
+</script>
+
+<style scoped>
+
+
+@media print {
+  .header {
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+}
+
+@media print {
+  .footer {
+    display: block;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+}
+
+
+</style>
