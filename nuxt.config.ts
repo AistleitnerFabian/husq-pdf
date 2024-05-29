2c829905894293d178f692c9b453850a29fc5e9c
Florian Rohrauer <florian.rohrauer@objectbay.com>
2024-05-29 20:53:35 +0200

    nuxt config for building

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
    modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint"],
-   devtools: { enabled: true },
-
+   devtools: {
+      enabled: true,
+   },
    dir: {
       pages: "documents",
       static: "schema",
    },
    vite: {
+      build: {
+         target: "esnext",
+      },
       optimizeDeps: {
          esbuildOptions: {
             target: "esnext",
          },
       },
    },
-
    nitro: {
       preset: "bun",
       experimental: {
          openAPI: true,
+      },
+      output: {
+         dir: "dist",
       },
    },
 });
