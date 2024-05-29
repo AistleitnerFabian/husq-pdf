a1a071f1bd6d216637cfd9c20a09a443f0ab0858
Florian Rohrauer <florian.rohrauer@objectbay.com>
2024-05-29 09:04:17 +0200

    implemented nested properties in editor

--- components/header/app-header.vue
+++ components/header/app-header.vue
@@ -2,7 +2,7 @@
   <div class="h-12 bg-background flex w-full items-center justify-between">
     <div class="flex items-center gap-2">
       <div class="border rounded size-8 flex justify-center items-center">
-        <span class="-translate-y-0.5">qp</span>
+        <span class="-translate-y-0.5 text-primary">qp</span>
       </div>
       <p>Husq.pdf</p>
     </div>
