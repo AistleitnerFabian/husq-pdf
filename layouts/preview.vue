dd7d5e9bf831976abac8479213f998a8067b114c
Fabian Aistleitner <fabian.aistleitner@objectbay.com>
2024-05-29 10:27:05 +0200

    add scroll view and update pending state ui

--- layouts/preview.vue
+++ layouts/preview.vue
@@ -1,11 +1,18 @@
 <template>
    <div class="size-full">
-      <div v-if="pending" class="space-y-4 p-8">
+     <div v-if="pending" class="absolute z-10 left-1/2 mt-6 -translate-x-1/2">
+        <div class="bg-background flex gap-2 items-center p-2 px-4 border rounded-full animate-in slide-in-from-top-3">
+          <Loader2 class="size-3 animate-spin text-primary"/>
+          <span class="text-sm">Refreshing document...</span>
+        </div>
+     </div>
+
+      <div v-if="pending && !data" class="space-y-4 p-8">
          <Skeleton class="mx-auto h-[842px] w-[595px]" />
          <Skeleton class="mx-auto h-[842px] w-[595px]" />
       </div>
 
-      <div v-if="!pending && data" class="h-full w-full overflow-y-auto p-8">
+      <ScrollArea v-else class="h-full w-full p-8">
          <div v-for="(_, index) in data.pages" :key="index" class="mb-4">
             <VuePDF
                :pdf="data.pdf"
@@ -13,7 +20,7 @@
                class="!flex justify-center drop-shadow-xl"
             />
          </div>
-      </div>
+      </ScrollArea>
    </div>
 </template>
 
@@ -21,6 +28,7 @@
 import { usePDF, VuePDF } from "@tato30/vue-pdf";
 import { usePageData } from "~/composables/usePageData";
 import { Skeleton } from "~/components/ui/skeleton";
+import { Loader2 } from 'lucide-vue-next'
 
 async function getPdfFile(): Promise<any> {
    const pageData = usePageData();
