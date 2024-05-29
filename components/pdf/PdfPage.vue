7238e6d14668db00c952dd2607891244e5a1ce71
Florian Rohrauer <florian.rohrauer@objectbay.com>
2024-05-29 14:40:27 +0200

    pdf page improvements

--- components/pdf/PdfPage.vue
+++ components/pdf/PdfPage.vue
@@ -1,30 +1,37 @@
 <template>
-   <div class="break-before-page">
-      <div class="content">
-         <slot />
-      </div>
+   <div :class="name">
+      <slot />
    </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
+   name: string;
    headerHeight: string;
    format?: "A4" | "A3" | "A5";
    orientation?: "portrait" | "landscape";
 }>();
 
+const name = props.name;
 const format = props.format ?? "A4";
 const orientation = props.orientation ?? "portrait";
 
 const injectPrintStyles = () => {
    const styleElement = document.createElement("style");
    styleElement.innerHTML = `
-    @media print {
-      @page {
-        size: ${format} ${orientation};
-        margin: ${props.headerHeight};
+      @media print {
+        @page ${name} {
+          size: ${format} ${orientation};
+          margin-top: ${props.headerHeight};
+          page-break-after: avoid !important;
+          page-break-before: avoid !important;
+          page-break-inside: avoid !important;
+        }
       }
-    }
+
+      .${name} {
+        page: ${name};
+      }
   `;
    document.head.appendChild(styleElement);
 };
